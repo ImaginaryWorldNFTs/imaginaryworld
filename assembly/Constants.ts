3c3ad884392887d7ecda29038e24c07e6ade7da8
jpw <user@user.com>
2024-04-03 10:22:06 -0400

    initial commit

---
+++ assembly/Constants.ts
@@ -0,0 +1,15 @@
+import { Base58 } from "@koinos/sdk-as";
+
+export namespace Constants {
+  export const NAME: string = "Imaginary World";
+  export const SYMBOL: string = "IMW";
+  export const MINT_PRICE: u64 = 1061500000;
+  export const MINT_FEE: bool = true;
+  export const MAX_SUPPLY: u64 = 55;
+  export const URI: string = "https://nftstorage.link/ipfs/bafybeihd5tqzmrbs4ilk4oggsyw4ifghw7dnqevxvm7wexzcfg7ua3shwa";
+  export const OWNER: Uint8Array = Base58.decode("1EwYTYLrxN8iyXTh1Acso9LgV5TGP9e1ey");
+
+  // token mint
+  export const TOKEN_PAY: Uint8Array = Base58.decode("15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL");
+  export const ADDRESS_PAY: Uint8Array = Base58.decode("1CKPdBdh29pTXrUV4UNtVnNDyUBzxfg4U3");
+}
